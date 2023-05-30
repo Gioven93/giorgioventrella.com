11e1291d6116478304c1895a28f2d99309737667
giorgio ventrella <gioschy93@gmail.com>
2023-05-30 17:19:27 +0200

    pointing to the actual domain

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "872ad374b489e782"; }
+/******/ 		__webpack_require__.h = function() { return "56c032b4e8daf6ea"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
